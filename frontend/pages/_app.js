05d0c1bd2bc57c347ac962cb47cd73c41da4b33f
Dale Salter <dalejsalter@outlook.com>
2020-09-01 16:56:50 +1000

    ADDED: GA

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,11 @@
-import App from 'next/app'
+import { useEffect } from 'react';
 
 import { ThemeProvider } from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css'
+
+import * as gtag from '../libs/gtag'
+import Router from 'next/router'
 
 const theme = {
   colors: {
@@ -12,15 +15,23 @@ const theme = {
 
 import { withApollo } from '../libs/with-apollo';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
-  }
+const App = ({ Component, pageProps }) => {
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default withApollo({ ssr: true })(MyApp);
+export default withApollo({ ssr: true })(App);
